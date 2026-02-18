e06ceac3d8f0936805e9a3adffb29f0287646828
monions23 <onionsmary7195@gmail.com>
2026-02-17 18:37:57 -0600

    worked on connecting recipe API to cards

---
+++ js/fetch_spoonacular.js
@@ -0,0 +1,61 @@
+const apiKey = '2f828b7f1c5e4eaf98fe8c55fd825aa0';
+const url = 'https://api.spoonacular.com/recipes/complexSearch?includeIngredients=leeks&fill\
+            Ingredients=true&addRecipeInformation=true&addRecipeInstructions=true&number=12';
+
+const cardContainer = document.querySelector('.recipe-grid');
+
+async function fetchRecipes() {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'x-api-key': apiKey,
+                'Content-Type': 'application/json'
+            }
+        })
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        var data = await response.json()
+            .then(
+                data => {
+                    console.log(data.results);
+                    displayCards(data.results);
+                }
+            );
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayCards(cardsData) {
+
+  cardsData.forEach(cardData => {
+
+    // Create card elements
+    var card = document.createElement('div');
+    var recipeImage = document.createElement('img');
+    var recipeTitle = document.createElement('div');
+
+    // Add classes for styling
+    card.classList.add('card');
+    card.classList.add('recipe');
+    recipeImage.classList.add('recipe-image');
+    recipeTitle.classList.add('recipe-text');
+
+    // Set content dynamically
+    recipeImage.src = cardData.image;
+    recipeImage.alt = cardData.title;
+    recipeTitle.textContent = cardData.title;
+
+    // Append elements to the card, and the card to the container
+    card.appendChild(recipeImage);
+    card.appendChild(recipeTitle);
+    cardContainer.appendChild(card);
+  })
+}
+
+
+fetchRecipes();
