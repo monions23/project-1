568896d00992804150e8e39ae5542280083ea2eb
monions23 <onionsmary7195@gmail.com>
2026-02-23 21:47:57 -0600

    Queried ingredients and passed recipe information to cards

--- js/fetch_spoonacular.js
+++ js/fetch_spoonacular.js
@@ -2,17 +2,20 @@ const apiKey1 = '2f828b7f1c5e4eaf98fe8c55fd825aa0';
 const apiKey2 = 'ec189335398d41cca8df74580c3d7f76';
 const apiKey3 = '0d22fdea05784a548001de5aa76848e8';
 
-const url = 'https://api.spoonacular.com/recipes/complexSearch?includeIngredients=leeks&fill\
-            Ingredients=true&addRecipeInformation=true&addRecipeInstructions=true&number=12';
-
 const cardContainer = document.querySelector('.recipe-grid');
 
-async function fetchRecipes() {
+async function fetchIngredients(produceArr) {
     try {
+        /* fetch produce and output cards */
+        var produceStr = produceArr.join(",")
+        var encodedProduceStr = encodeURIComponent(produceStr);
+        var url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodedProduceStr}&\
+        number=12&ranking=1`;
+
         const response = await fetch(url, {
             method: 'GET',
             headers: {
-                'x-api-key': apiKey1,
+                'x-api-key': apiKey2,
                 'Content-Type': 'application/json'
             }
         })
@@ -23,8 +26,13 @@ async function fetchRecipes() {
         var data = await response.json()
             .then(
                 data => {
-                    console.log(data.results);
-                    displayCards(data.results);
+                    var recipeIDs = [];
+                    console.log(data);
+                    data.forEach(recipe => {
+                        recipeIDs.push(recipe.id);
+                    })
+                    //displayProduceCards(data, produceArr);
+                    fetchRecipes(data, recipeIDs);
                 }
         );
     }
@@ -33,7 +41,64 @@ async function fetchRecipes() {
     }
 }
 
-function displayCards(cardsData) {
+async function fetchRecipes(data, recipeIDs) {
+    /* Fetch recipes and output cards */
+    try {
+        console.log(recipeIDs);
+        let recipeIDsStr = recipeIDs.join(",");
+        console.log(recipeIDsStr);
+        var url = `https://api.spoonacular.com/recipes/informationBulk?ids=${recipeIDsStr}`;
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'x-api-key': apiKey2,
+                'Content-Type': 'application/json'
+            }
+        })
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        var data = await response.json()
+            .then(
+                data => {
+                    console.log(data);
+                    displayRecipeCards(data);
+                }
+        );
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+// function displayProduceCards(produceData, produceArr) {
+
+
+//     // Create card elements
+//     var card = document.createElement('div');
+//     var recipeImage = document.createElement('img');
+//     var recipeTitle = document.createElement('div');
+
+//     // Add classes for styling
+//     card.classList.add('card');
+//     card.classList.add('recipe');
+//     recipeImage.classList.add('recipe-image');
+//     recipeTitle.classList.add('recipe-text');
+
+//     // Set content dynamically
+//     recipeImage.src = produceData.image;
+//     recipeImage.alt = produceData.title;
+//     recipeTitle.textContent = produceData.title;
+
+//     // Append elements to the card, and the card to the container
+//     card.appendChild(recipeImage);
+//     card.appendChild(recipeTitle);
+//     cardContainer.appendChild(card);
+// }
+
+function displayRecipeCards(cardsData) {
 
   cardsData.forEach(cardData => {
 
@@ -59,6 +124,3 @@ function displayCards(cardsData) {
     cardContainer.appendChild(card);
   })
 }
-
-
-fetchRecipes();
--- js/index.js
+++ js/index.js
@@ -1,4 +1,47 @@
+// ali's code:
+let data = null;
+
+fetch("data.json")
+  .then(response => response.json())
+  .then(json => {
+    data = json;
+    console.log("Data loaded:", data);
+  })
+  .catch(err => console.error("Failed to load data.json", err));
+  
+
+function selectProduce() {
+
+  // Select all elements that match the CSS selector
+  const cardElements = document.querySelectorAll(".card");
+
+  // Iterate over the resulting NodeList and remove each element
+  for (const card of cardElements) {
+    card.remove();
+  }
+
+  const stateElement = document.getElementById("states-select");
+  const seasonElement = document.getElementById("seasons-select");
+
+  const stateChoice = stateElement.value;
+  const seasonChoice = seasonElement.value;
+
+  const region = data.regions.find(r =>
+    r.states.includes(stateChoice)
+  );
+
+  if (!region) return [];
+
+  console.log(region.seasons[seasonChoice] || [])
+
+  fetchIngredients(region.seasons[seasonChoice] || []);
+  return region.seasons[seasonChoice] || [];
+
+
+};
+
 document.addEventListener("DOMContentLoaded", function () {
+
     const slider = document.querySelector(".slider");
     const nextBtn = document.querySelector(".next-btn");
     const prevBtn = document.querySelector(".prev-btn");
@@ -39,33 +82,3 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!inside) modal.close();
   });
 });
-
-// ali's code:
-let data = null;
-
-fetch("data.json")
-  .then(response => response.json())
-  .then(json => {
-    data = json;
-    console.log("Data loaded:", data);
-  })
-  .catch(err => console.error("Failed to load data.json", err));
-  
-function selectProduce() {
-
-  const stateElement = document.getElementById("states-select");
-  const seasonElement = document.getElementById("seasons-select");
-
-  const stateChoice = stateElement.value;
-  const seasonChoice = seasonElement.value;
-
-  const region = data.regions.find(r =>
-    r.states.includes(stateChoice)
-  );
-
-  if (!region) return [];
-
-  return region.seasons[seasonChoice] || [];
-
-
-};
